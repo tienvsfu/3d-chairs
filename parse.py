828f6ae1391209bba2eb99af9966289690b0c3dd
tselun <tsc8@sfu.ca>
2020-04-11 15:53:39 -0700

    random mix

--- parse.py
+++ parse.py
@@ -1,3 +1,25 @@
 import trimesh
-mesh = trimesh.load('chairs/173/back.obj')
-mesh.show()
+import random 
+
+n = 10
+c = [173,347,470,515,688,1095,1325,2820,3001,39101]
+a = random.randint(0,n-1)
+b = random.randint(0,n-1)
+l = random.randint(0,n-1)
+s = random.randint(0,n-1)
+
+arm_exist = True
+try: 
+    arm = trimesh.load('chairs/'+str(c[a])+'/arm.obj')
+except:
+    arm_exist = False
+
+back = trimesh.load('chairs/'+str(c[b])+'/back.obj')
+leg = trimesh.load('chairs/'+str(c[l])+'/leg.obj')
+seat = trimesh.load('chairs/'+str(c[s])+'/seat.obj')
+
+if arm_exist==True:
+    scenes = trimesh.Scene([arm,back,leg,seat])
+else:
+    scenes = trimesh.Scene([back,leg,seat])
+scenes.show()
