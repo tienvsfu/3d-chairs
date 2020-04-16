a05f214a16c6c7e2234dde62f0827a56e6a0db62
tselun <tsc8@sfu.ca>
2020-04-15 23:20:46 -0700

    revert too confusing

--- m.py
+++ m.py
@@ -1,10 +1,15 @@
 
+# import os
+# import ntpath
+# import sys
+# import shutil
+# import params
+# from scorer import evaluate
 
 import re
 
 from p import *
 from mm import *
-#from s import *
 
 # load
 c1 = input("Please enter test IDs (e.g. 2585, 2323, 43872):\n")
@@ -15,7 +20,26 @@ for test_case in selected_test_cases:
 
 parse(c1)
 generate(10,c2)
+
 #score()
+obj_dirs = params.obj_dirs
+score_dir = params.score_dir
+
+# #scorer
+# #results is a dict with structure: {'mm_a':{0 : probability_0, 1:probability_1}, 'mm_b':{0: probability_0, 1: probability_1}}
+# results = {}
+# for obj_dir in obj_dirs:
+#     base_name =  os.path.basename(os.path.normpath(obj_dir))
+#     sorted_result = evaluate.evaluate(obj_dir)
+#     results[base_name] = sorted_result
+# #Write to txt file
+# if os.path.exists(score_dir):
+#     shutil.rmtree(score_dir)
+# os.mkdir(score_dir)
+# for base_name in results:
+#     print(base_name)
+#     score_file_dir = os.path.join(score_dir, base_name + '.txt')
+#     evaluate.export_results(results[base_name], score_file_dir)
 
 # display
 ranking = []
